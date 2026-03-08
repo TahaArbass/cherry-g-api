45c571cf05bff3e758567522c8a481b4c03b0170
TahaArbass <tahaa2377@gmail.com>
2026-03-08 16:44:20 +0200

    file name fix

--- main.c
+++ main.c
@@ -13,11 +13,11 @@ int main() {
     const char *outputPath = buf;
     const int scale = 60;
     // .ppm starts as a text format then switch to binary format
-    FILE *f = fopen(outputPath, "wb");
+    FILE *file = fopen(outputPath, "wb");
     int w = 16 * scale, h = 9 * scale;
-    fprintf(f, "P6\n");
-    fprintf(f, "%d %d\n", w, h);
-    fprintf(f, "255\n");
+    fprintf(file, "P6\n");
+    fprintf(file, "%d %d\n", w, h);
+    fprintf(file, "255\n");
 
     vec2 r = {(float)w, (float)h}; // screen res
     for (int y = 0; y < h; y++) {
@@ -58,12 +58,12 @@ int main() {
 
         v = p + cos(i * i + (t + p.x * .1) * .03 + i * vec2(11, 9)) * 5.;
         o = tanh(pow(vec4_divf(o, 1e2), vec4(1.5)));
-        fputc(o.x * 255, f);
-        fputc(o.y * 255, f);
-        fputc(o.z * 255, f);
+        fputc(o.x * 255, file);
+        fputc(o.y * 255, file);
+        fputc(o.z * 255, file);
       }
     }
-    fclose(f);
+    fclose(file);
     printf("Generated %s\n", outputPath);
   }
 
